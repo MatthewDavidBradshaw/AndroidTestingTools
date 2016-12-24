d47880a2191dea63647149c6accb23bc70ac0cc3
Matthew Tamlin <matthew.tamlin@icloud.com>
2016-12-24 18:06:14 +1030

    Created DemoTestHarness.java

--- sample/src/main/java/com/matthewtamlin/android_testing_tools/sample/DemoTestHarness.java
+++ sample/src/main/java/com/matthewtamlin/android_testing_tools/sample/DemoTestHarness.java
@@ -1,8 +1,46 @@
 package com.matthewtamlin.android_testing_tools.sample;
 
-/**
- * Created by matt on 16/12/24.
- */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 
-public class DemoTestHarness {
+import com.matthewtamlin.android_testing_tools.library.ControlsOverViewTestHarness;
+
+/**
+ * Displays a DemoCustomView and some control buttons.
+ */
+public class DemoTestHarness extends ControlsOverViewTestHarness<DemoCustomView> {
+	private DemoCustomView testView;
+
+	@Override
+	protected void onCreate(final @Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		getControlsContainer().addView(createControlButtonToSetText());
+	}
+
+	@Override
+	public DemoCustomView getTestView() {
+		if (testView == null) {
+			testView = new DemoCustomView(this);
+		}
+
+		return testView;
+	}
+
+	private Button createControlButtonToSetText() {
+		final Button b = new Button(this);
+		b.setText("Set text \"Christmas\"");
+		b.setAllCaps(false);
+
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				getTestView().setText("Christmas");
+			}
+		});
+
+		return b;
+	}
 }
