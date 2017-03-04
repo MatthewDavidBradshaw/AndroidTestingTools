3783e26fe5a4b4da7b328a39720039b886d2357b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-04 14:17:53 +1030

    Updated Javadoc and renamed member variable

--- library/src/main/java/com/matthewtamlin/android_testing_tools/library/espresso/TypeSafeViewAssertion.java
+++ library/src/main/java/com/matthewtamlin/android_testing_tools/library/espresso/TypeSafeViewAssertion.java
@@ -5,29 +5,83 @@ import android.support.test.espresso.ViewAssertion;
 import android.view.View;
 
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static com.matthewtamlin.android_testing_tools.library.espresso.CustomMatchers.isExactType;
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+/**
+ * A ViewAssertion with built in type safety.
+ * <p>
+ * Subclasses can define the action to perform by implementing {@link #typeSafeCheck(View,
+ * NoMatchingViewException)}. If subclasses cannot avoid overriding {@link #check(View,
+ * NoMatchingViewException)}, then the superclass implementation must be called from within teh
+ * override.
+ *
+ * @param <T>
+ * 		the type of View this assertion can be performed on
+ */
 public abstract class TypeSafeViewAssertion<T extends View> implements ViewAssertion {
-	private final Class<T> clazz;
+	/**
+	 * The class this action can be applied to.
+	 */
+	private final Class<T> targetClass;
 
-	public TypeSafeViewAssertion(Class<T> clazz) {
-		this.clazz = checkNotNull(clazz, "clazz cannot be null.");
+	/**
+	 * Whether or not this action can also be applied to subclasses of the target class.
+	 */
+	private boolean allowSubclasses;
+
+	/**
+	 * Constructs a new TypeSafeViewAssertion.
+	 *
+	 * @param targetClass
+	 * 		the class this view assertion can be applied to
+	 * @param allowSubclasses
+	 * 		whether or not this assertion can also be applied to subclasses of the target class
+	 * @throws IllegalArgumentException
+	 * 		if {@code targetClass} is null
+	 */
+	public TypeSafeViewAssertion(final Class<T> targetClass, final boolean allowSubclasses) {
+		this.targetClass = checkNotNull(targetClass, "targetClass cannot be null.");
+		this.allowSubclasses = allowSubclasses;
 	}
 
 	@Override
 	public void check(final View view, final NoMatchingViewException noViewFoundException) {
-		if (view == null) {
-			throw noViewFoundException;
-		}
-
-		if (!isAssignableFrom(clazz).matches(view)) {
-			throw new IllegalArgumentException("view must be an instance of or subclass of " +
-					clazz.getName());
+		if (view != null) {
+			checkConstraints(view);
 		}
 
 		//noinspection unchecked - This is prevented by the above check
-		typeSafeCheck((T) view);
+		typeSafeCheck((T) view, noViewFoundException);
 	}
 
-	public abstract void typeSafeCheck(T view);
+	/**
+	 * Checks the state of the given view (if such a view is present).
+	 *
+	 * @param view
+	 * 		the view if one was found during the view interaction, or null if it was not
+	 * @param noViewFoundException
+	 * 		an exception detailing why the view could not be found, or null if the view was found
+	 */
+	public abstract void typeSafeCheck(T view, NoMatchingViewException noViewFoundException);
+
+	/**
+	 * Checks that the supplied view can be used with this view assertion. If the check fails then a
+	 * detailed exception will be thrown. If the check passes, the method will exit normally.
+	 *
+	 * @param view
+	 * 		the view to check
+	 */
+	private void checkConstraints(final View view) {
+		if (allowSubclasses) {
+			if (!isAssignableFrom(targetClass).matches(view)) {
+				throw new RuntimeException("view must be an instance of" + targetClass.getName());
+			}
+		} else {
+			if (!isExactType(targetClass).matches(view)) {
+				throw new RuntimeException("view must be an instance of " + targetClass.getName() +
+						" (excluding subclasses)");
+			}
+		}
+	}
 }
