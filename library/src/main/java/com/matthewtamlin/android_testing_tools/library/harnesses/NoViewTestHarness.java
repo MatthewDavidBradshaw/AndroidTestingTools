6a72b6c6769b58cf541fdd01f18b5d45f5141cb0
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-16 18:08:00 +1030

    Deleted @Registered annotation

--- library/src/main/java/com/matthewtamlin/android_testing_tools/library/harnesses/NoViewTestHarness.java
+++ library/src/main/java/com/matthewtamlin/android_testing_tools/library/harnesses/NoViewTestHarness.java
@@ -16,14 +16,12 @@
 
 package com.matthewtamlin.android_testing_tools.library.harnesses;
 
-import android.annotation.SuppressLint;
 import android.view.View;
 
 /**
  * A TestHarness which displays no view. Useful when a real Activity is needed in testing, but no
  * view is required.
  */
-@SuppressLint("Registered") // Class is part of a public API and is not instantiated in this project
 public class NoViewTestHarness extends ControlsOverViewTestHarness<View> {
 	@Override
 	public View getTestView() {
