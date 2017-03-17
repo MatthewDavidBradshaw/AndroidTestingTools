dc674bcc3a1ac94020210bafb65cc6171fad18a4
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-17 13:57:39 +1030

    Deleted unnecessary test case

--- testing/src/test/java/com/matthewtamlin/android_testing_tools/testing/harnesses/TestControlBinder.java
+++ testing/src/test/java/com/matthewtamlin/android_testing_tools/testing/harnesses/TestControlBinder.java
@@ -103,39 +103,6 @@ public class TestControlBinder {
 	}
 
 	@Test
-	public void testBindControls_annotationOnMultipleValidMethods() {
-		final View view1 = mock(View.class);
-		final View view2 = mock(View.class);
-		final View view3 = mock(View.class);
-
-		final StubTestHarness testHarness = new StubTestHarness() {
-			@Control(2)
-			public View someMethod2() {
-				return view2;
-			}
-
-			@Control(1)
-			public View someMethod1() {
-				return view1;
-			}
-
-			@Control(3)
-			public View someMethod3() {
-				return view3;
-			}
-		};
-
-		ControlBinder.bindControls(testHarness);
-
-		final List<View> expectedViews = new ArrayList<>();
-		expectedViews.add(view1);
-		expectedViews.add(view2);
-		expectedViews.add(view3);
-
-		assertThat(testHarness.getControls(), is(expectedViews));
-	}
-
-	@Test
 	public void testBindControls_noAnnotations() {
 		final StubTestHarness testHarness = new StubTestHarness();
 
