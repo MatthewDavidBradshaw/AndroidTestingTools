e216769b5d6eee120cea1fd4bd4aaa75d0e21b08
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-17 14:17:47 +1030

    Updated exception message

--- library/src/main/java/com/matthewtamlin/android_testing_tools/library/harnesses/ControlBinder.java
+++ library/src/main/java/com/matthewtamlin/android_testing_tools/library/harnesses/ControlBinder.java
@@ -37,7 +37,7 @@ public class ControlBinder {
 					control = (View) m.invoke(testHarness);
 				} catch (final IllegalAccessException e) {
 					throw new RuntimeException("Unable to access a method annotated with " +
-							"@Control. The method must be public or protected.", e);
+							"@Control. The method must be public.", e);
 				} catch (final InvocationTargetException e) {
 					throw new RuntimeException("A method annotated with @Control threw an " +
 							"exception when invoked.", e);
