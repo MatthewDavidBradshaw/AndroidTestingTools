94f785b5709cc81f5da8642b535b9a9ce7fe758b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-18 12:16:29 +1030

    Added tests for getters

--- testing/src/androidTest/java/com/matthewtamlin/android_testing_tools/testing/espresso/TestControlsAboveViewTestHarness.java
+++ testing/src/androidTest/java/com/matthewtamlin/android_testing_tools/testing/espresso/TestControlsAboveViewTestHarness.java
@@ -1,11 +1,20 @@
 package com.matthewtamlin.android_testing_tools.testing.espresso;
 
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
 
 import com.matthewtamlin.android_testing_tools.testing.StubControlsAboveViewTestHarness;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+
+import static com.matthewtamlin.android_testing_tools.library.R.id.controlsAboveView_innerControls;
+import static com.matthewtamlin.android_testing_tools.library.R.id.controlsAboveView_outerControls;
+import static com.matthewtamlin.android_testing_tools.library.R.id.controlsAboveView_root;
+import static com.matthewtamlin.android_testing_tools.library.R.id.controlsAboveView_testViewContainer;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class TestControlsAboveViewTestHarness {
 	@Rule
@@ -17,5 +26,29 @@ public class TestControlsAboveViewTestHarness {
 	@Before
 	public void setup() {
 		activity = rule.getActivity();
+	}
+
+	@Test
+	public void testGetRootView() {
+		final View expectedView = activity.findViewById(controlsAboveView_root);
+		assertThat(activity.getRootView(), is(expectedView));
+	}
+
+	@Test
+	public void testGetInnerControlsContainer() {
+		final View expectedView = activity.findViewById(controlsAboveView_innerControls);
+		assertThat(activity.getRootView(), is(expectedView));
+	}
+
+	@Test
+	public void testGetOuterControlsContainer() {
+		final View expectedView = activity.findViewById(controlsAboveView_outerControls);
+		assertThat(activity.getRootView(), is(expectedView));
+	}
+
+	@Test
+	public void testGetTestViewContainer() {
+		final View expectedView = activity.findViewById(controlsAboveView_testViewContainer);
+		assertThat(activity.getRootView(), is(expectedView));
 	}
 }
