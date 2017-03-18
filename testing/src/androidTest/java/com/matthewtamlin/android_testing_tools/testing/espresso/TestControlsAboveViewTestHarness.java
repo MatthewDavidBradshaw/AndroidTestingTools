27a0b67928ad4751e97bba5aa51c823f63514ce5
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-18 12:51:14 +1030

    Added test for initial state

--- testing/src/androidTest/java/com/matthewtamlin/android_testing_tools/testing/espresso/TestControlsAboveViewTestHarness.java
+++ testing/src/androidTest/java/com/matthewtamlin/android_testing_tools/testing/espresso/TestControlsAboveViewTestHarness.java
@@ -2,6 +2,7 @@ package com.matthewtamlin.android_testing_tools.testing.espresso;
 
 import android.support.test.rule.ActivityTestRule;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.matthewtamlin.android_testing_tools.testing.StubControlsAboveViewTestHarness;
 
@@ -9,6 +10,11 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.Visibility.VISIBLE;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.matthewtamlin.android_testing_tools.library.R.id.controlsAboveView_innerControls;
 import static com.matthewtamlin.android_testing_tools.library.R.id.controlsAboveView_outerControls;
 import static com.matthewtamlin.android_testing_tools.library.R.id.controlsAboveView_root;
@@ -26,6 +32,23 @@ public class TestControlsAboveViewTestHarness {
 	@Before
 	public void setup() {
 		activity = rule.getActivity();
+	}
+
+	@Test
+	public void testInitialState() {
+		final LinearLayout controls = activity.getInnerControlsContainer();
+
+		// Check all expected controls are present
+		assertThat("Incorrect number of controls.", controls.getChildCount(), is(3));
+		assertThat(controls.getChildAt(0), is(activity.controlView1));
+		assertThat(controls.getChildAt(1), is(activity.controlView2));
+		assertThat(controls.getChildAt(2), is(activity.controlView3));
+
+		// Check all controls are visible
+		onView(withId(controlsAboveView_outerControls))
+				.check(matches(withEffectiveVisibility(VISIBLE)));
+		onView(withId(controlsAboveView_innerControls))
+				.check(matches(withEffectiveVisibility(VISIBLE)));
 	}
 
 	@Test
