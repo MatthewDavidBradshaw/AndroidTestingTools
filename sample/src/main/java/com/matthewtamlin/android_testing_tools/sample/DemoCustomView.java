d6a6d0ac27084c4368ffbe742fa92cab519027bc
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-18 16:17:39 +1030

    Changed contents

--- sample/src/main/java/com/matthewtamlin/android_testing_tools/sample/DemoCustomView.java
+++ sample/src/main/java/com/matthewtamlin/android_testing_tools/sample/DemoCustomView.java
@@ -21,29 +21,14 @@ import android.view.View;
 import android.widget.TextView;
 
 /**
- * A simple custom TextView subclass. This could easily be unit tested without using the UI, but for
- * the purposes of the demonstration we will overlook this.
+ * Some custom view.
  */
 public class DemoCustomView extends View {
-	/**
-	 * Constructs a new DemoCustomView.
-	 *
-	 * @param context
-	 * 		the Context the view is operating in, not null
-	 */
 	public DemoCustomView(final Context context) {
 		super(context);
 	}
 
-	/**
-	 * Checks that the view contains the supplied text.
-	 *
-	 * @param textToCheck
-	 * 		some text
-	 * @return true if the view currently contains the supplied text, false otherwise
-	 */
-	public boolean textContains(final String textToCheck) {
-		final String textInView = (String) getText();
-		return textInView.contains(textToCheck);
+	public void someCustomMethod() {
+		// Do something
 	}
 }
