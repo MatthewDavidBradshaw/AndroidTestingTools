47d9be99d7e85fbc6996d2f8c39999d5a04392e1
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-18 14:48:56 +1030

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/android_testing_tools/library/harnesses/Control.java
+++ library/src/main/java/com/matthewtamlin/android_testing_tools/library/harnesses/Control.java
@@ -5,8 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a method which returns a control view for a TestHarness. This annotation must
+ * only be applied to methods which are:
+ * <ul> <li>Public</li> <li>Have no arguments</li> <li>Return a non-null View.</li></ul>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Control {
+	/**
+	 * @return the index of the control
+	 */
 	int value();
 }
